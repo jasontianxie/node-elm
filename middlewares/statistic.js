508f5d0dd5afd0a2e77cb6a6b51a8840cea99883
jasontianxie <1986tianxie@sina.com>
2017-08-09 22:59:49 +0800

    Update statistic.js

--- middlewares/statistic.js
+++ middlewares/statistic.js
@@ -2,7 +2,7 @@
 
 import BaseComponent from '../prototype/baseComponent'
 import StatisModel from '../models/statis/statis'
-import dtime from 'time-formater'
+import dtime from 'time-formater'//返回时间的格式，具体用法看npm
 
 class Statistic extends BaseComponent {
 	constructor(){
@@ -11,7 +11,7 @@ class Statistic extends BaseComponent {
 	}
 	async apiRecord(req, res, next){
 		try{
-			const statis_id = await this.getId('statis_id')
+			const statis_id = await this.getId('statis_id')//getId方法在BaseComponent中定义的
 			const apiInfo = {
 				date: dtime().format('YYYY-MM-DD'),
 				origin: req.headers.origin,
